375c6994c47fb841ba0503d1a6b594a18872087b
Fabio Rehm <fgrehm@gmail.com>
2014-10-12 21:30:19 -0300

    Page views

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,16 @@
 package controllers;
 
+import java.util.Date;
 import models.PageView;
-import play.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
     public static Result index() {
-        return ok(index.render("Your new application is ready. + " + PageView.find.all()));
+        PageView pageView = new PageView();
+        pageView.date = new Date();
+        pageView.save();
+        return ok(index.render(PageView.find.findRowCount()));
     }
 }
